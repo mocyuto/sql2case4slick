e69be97458846070aa6f20036e273500807a5653
Yuto Suzuki <yuutoo.advance@gmail.com>
2015-12-02 12:35:13 +0900

    remove primary key column and add astarisk

--- sql2case_v2.py
+++ sql2case_v2.py
@@ -19,11 +19,14 @@ class Table:
 
     def caseClass(self):
         print "case class " + self.camelTable + caseSuffix + "("
+        valList = []
         for i,elem in enumerate(self.columnSeq):
-            columnItems = elem.split()
-            columnName = re.sub("`" ,"" , columnItems[0]) 
-            print "  " + snake2camel(columnName) + ": " + datatype(columnItems[1]),
-            if (i < len(self.columnSeq) - 1):
+            columnName, dataType = columnData(elem)
+            if dataType != "":
+                valList.append("  " + snake2camel(columnName) + ": " + dataType)
+        for i,val in enumerate(valList):
+            print val,
+            if (i < len(valList) - 1):
                 print","
             else:
                 print
@@ -31,11 +34,14 @@ class Table:
 
     def tableTag(self):
         print "class " + self.camelTable + "Table(tag: Tag) extends Table[" + self.camelTable + caseSuffix +"](tag, \"" + self.tableName + "\") {"
+            
         for i,elem in enumerate(self.columnSeq):
-            columnItems = elem.split()
-            columnName = re.sub("`" ,"" , columnItems[0])
-            dataType = datatype(columnItems[1])
-            print "  def " + snake2camel(columnName) + ": Column[" + dataType + "] = column[" + dataType + "](\"" + columnName + "\")"
+            columnName, dataType = columnData(elem)
+            if dataType != "":
+                print "  def " + snake2camel(columnName) + ": Column[" + dataType + "] = column[" + dataType + "](\"" + columnName + "\")"
+            
+        columns = [columnData(x)[0] for x in self.columnSeq if columnData(x)[1] != ""]
+        print "def * = (" + ",".join(columns) + ")<>(" + self.camelTable + caseSuffix + ".tuppled, " + self.camelTable + caseSuffix + ".unapply)"
         print "}"
 
     def getTableName(self):
@@ -48,14 +54,22 @@ class Table:
 def snake2camel(string):
     return re.sub("_(.)",lambda x:x.group(1).upper(),string)
 
+def columnData(elem):
+    columnItems = elem.split()
+    columnName = re.sub("`" ,"" , columnItems[0])
+    dataType = datatype(columnItems[1])
+    return (columnName, dataType)
+
 def datatype(typeName):
     t = re.sub("\(.*\)","",typeName.lower())
     if t in {"bigint", "int", "tinyint"}:
         return "Int"
     elif t in {"timestamp", "datetime"}:
         return "DateTime"
-    else:
+    elif t in {"varchar","text"}:
         return "String"
+    else:
+        return ""
 
 def parserAdder():
     parser = ArgumentParser(description = "SQL to scala case class for slick ORM")
@@ -69,6 +83,7 @@ if __name__ == "__main__":
     args = parserAdder()
     with open(args.filename) as f:
         sql = "".join([x.strip() for x in f.readlines()]);
+        print sql
         t = Table(sql)
         t.caseClass()
         t.tableTag()
